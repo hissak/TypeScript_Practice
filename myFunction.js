6a8660fa59c2314fa9b297c91763b6f59979b203
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-20 14:47:41 -0400

    Add type check to parameter in function definition

---
+++ myFunction.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(2);
+exports.default = addTwo;
---
+++ myFunction.ts
@@ -0,0 +1,8 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+addTwo(2); 
+
+
+export default addTwo;
