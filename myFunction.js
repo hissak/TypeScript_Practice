83f2e76af94fc5c5c3e07a0524bbbb86f5b7fa0c
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-20 14:51:14 -0400

    More practice defining type for parameters

--- myFunction.js
+++ myFunction.js
@@ -3,5 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
 }
+function getUpper(val) {
+    return val.toUpperCase();
+}
 addTwo(2);
+getUpper('hello');
 exports.default = addTwo;
--- myFunction.ts
+++ myFunction.ts
@@ -2,7 +2,18 @@ function addTwo(num: number) {
   return num + 2;
 }
 
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, password: string, 
+  isPaid: boolean) {
+  
+}
+
 addTwo(2); 
+getUpper('hello');
+signUpUser('John', 'johnappleseed@gmail.com', 'password', true);
 
 
 export default addTwo;
