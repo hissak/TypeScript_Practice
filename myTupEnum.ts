9c554b530dd5facb386a792fdc2800918bfdb489
hissak <hassan.a.issak@gmail.com>
2023-09-21 20:29:04 -0400

    define tuples

--- myTupEnum.ts
+++ myTupEnum.ts
@@ -1,3 +1,9 @@
 let tUser: [string, number, boolean] = ['John', 25, true];
 
 tUser = ['John', 25, true];
+
+let rgb: [number, number, number] = [255, 0, 0];
+
+type User = [number, string]
+
+const newUser: User = [1, 'John'];
