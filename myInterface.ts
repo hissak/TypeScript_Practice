e20deaf38c23a370cc8e55c13d481648f552f857
hissak <hassan.a.issak@gmail.com>
2023-09-21 21:01:29 -0400

    Complete first round of practice with interface

--- myInterface.ts
+++ myInterface.ts
@@ -2,10 +2,21 @@ interface User {
   email: string,
   userId: number,
   dbId?: number
+  startTrail: () => string
+  // startTrail(): string
+  getCoupon?: (coupon: string, value: number) => number
 }
 
 const user: User = {
   email: 'user@use.com',
   userId: 20,
-  dbId: 1
+  dbId: 1,
+  startTrail: () => {
+    return 'trail started';
+  },
+  getCoupon: (coupon: 'xyz' | string, value: 10 | number) => {
+    return 2
+  }
 }
+
+
